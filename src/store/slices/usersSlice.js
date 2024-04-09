e4421c2979c7d1056a223382276ebe7d50de8b55
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-04-09 11:07:48 +0530

    setup media project

---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: {
+    data: [],
+  },
+  reducers: {},
+});
+export const usersReducer = usersSlice.reducer;
